2cc6e2dfc9a31caf96ecfd50b969b8a32484fb66
Arif <80994072+arf-themascoteers@users.noreply.github.com>
2023-01-26 21:20:46 +1100

    done

--- train.py
+++ train.py
@@ -31,7 +31,7 @@ def train(device, ds):
             optimizer.step()
             optimizer.zero_grad()
             batch_number += 1
-            print(f'Epoch:{epoch + 1} (of {num_epochs}), Batch: {batch_number} of {n_batches}, Loss:{loss.item():.6f}')
+            #print(f'Epoch:{epoch + 1} (of {num_epochs}), Batch: {batch_number} of {n_batches}, Loss:{loss.item():.6f}')
 
     #print("Train done")
     end = time.time()
